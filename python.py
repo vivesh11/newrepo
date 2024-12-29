2bf7eb9643c24078892a1d44e9c623177d9779fd
Vivesh <vivesh236@gmail.com>
2024-12-29 20:47:13 +0530

    second commit added new line

--- python.py
+++ python.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
     num2 = 3
     result = add_two_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}")
+
+    print("second print statement") # Added this line
