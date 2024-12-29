907fea744023cb3c5e6e9b8d661645f384d9ad4f
Vivesh <vivesh236@gmail.com>
2024-12-29 18:57:11 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    return a + b
+
+# Example usage
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 3
+    result = add_two_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
